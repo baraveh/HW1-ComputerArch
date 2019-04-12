36244258f3e57ceda9a5be6407152bc1c41c5e15
baraveh <baraveh@gmail.com>
2019-04-12 22:01:22 +0300

    Messed with the tests a bit so it will be readable, seems to be a problem with the prediction logic - no progress atm :(

---
+++ cmake-build-debug/CMakeFiles/HW1_ComputerArch.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/c/Users/Bar The magical/Google Drive/Computer Architecture/HW1/HW1-ComputerArch/bp_main.c" "/mnt/c/Users/Bar The magical/Google Drive/Computer Architecture/HW1/HW1-ComputerArch/cmake-build-debug/CMakeFiles/HW1_ComputerArch.dir/bp_main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Bar The magical/Google Drive/Computer Architecture/HW1/HW1-ComputerArch/bp.cpp" "/mnt/c/Users/Bar The magical/Google Drive/Computer Architecture/HW1/HW1-ComputerArch/cmake-build-debug/CMakeFiles/HW1_ComputerArch.dir/bp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HW1_ComputerArch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW1_ComputerArch.dir/bp.cpp.o"
+  "CMakeFiles/HW1_ComputerArch.dir/bp_main.c.o"
+  "HW1_ComputerArch.pdb"
+  "HW1_ComputerArch"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/HW1_ComputerArch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
