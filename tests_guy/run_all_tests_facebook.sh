d5ea5e1bb4cea2cbc050e4fd5f349afa43b94cc1
ShaharGottlieb <shahargottlieb@gmail.com>
2019-04-13 16:28:52 +0300

    still trying to find the bug..

--- tests_guy/run_all_tests_facebook.sh
+++ tests_guy/run_all_tests_facebook.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-let "i = 1"
 for filename in tests/test*.in; do
     test_num=`echo $filename | cut -d'.' -f1`
-    #dos2unix ${filename}
+    dos2unix ${filename}
     ./bp_main $filename > ${test_num}Yours.out
-	echo "test number $i"
     diff ${test_num}.out ${test_num}Yours.out
-	let "i++"
 done
